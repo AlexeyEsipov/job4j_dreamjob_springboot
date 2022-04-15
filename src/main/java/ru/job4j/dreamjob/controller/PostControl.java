10027a1129e523079dc8683f3131106031de11a2
AlexeyEsipov <a_esipov@list.ru>
2022-04-15 23:24:46 +0300

    Создание вакансии

--- src/main/java/ru/job4j/dreamjob/controller/PostControl.java
+++ src/main/java/ru/job4j/dreamjob/controller/PostControl.java
@@ -2,8 +2,12 @@ package ru.job4j.dreamjob.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import ru.job4j.dreamjob.model.Post;
 import ru.job4j.dreamjob.store.PostStore;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PostControl {
@@ -20,5 +24,11 @@ public class PostControl {
     public String addPost(Model model) {
         model.addAttribute("post", new Post(0, "Заполните поле", "Заполните поле"));
         return "addPost";
+    }
+
+    @PostMapping("/createPost")
+    public String createPost(@ModelAttribute Post post) {
+        store.add(post);
+        return "redirect:/posts";
     }
 }
